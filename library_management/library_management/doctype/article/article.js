76d8e3a9cc544c74da08538ac2faf808fafc8e42
Billy Adwar <billyfranks98@gmail.com>
2023-04-14 10:57:50 +0300

    comments

--- library_management/library_management/doctype/article/article.js
+++ library_management/library_management/doctype/article/article.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2023, Billy Adwar and contributors
 // For license information, please see license.txt
 
+
+// Testing custom API's within Frappe 
 frappe.ui.form.on('Article', {
 	refresh: function(frm) {
 		frm.add_custom_button("Get Author Books",()=>{
