c58fc9c6e4e481337aa4f833df34ffc2b46598a7
Billy Adwar <billyfranks98@gmail.com>
2023-04-14 10:56:05 +0300

    testing custom buttons for an API

--- library_management/library_management/doctype/article/article.js
+++ library_management/library_management/doctype/article/article.js
@@ -2,7 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Article', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button("Get Author Books",()=>{
+			console.log(frm.doc.author);
+		},"ACTIONS")
+	}
 });
