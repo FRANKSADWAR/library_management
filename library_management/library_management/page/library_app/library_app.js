9ab138667d66351cd0d8f2f52107d0e09f8f92f0
FRANKSADWAR <billyfranks98@gmail.com>
2023-04-20 13:48:40 +0300

    card content custom

--- library_management/library_management/page/library_app/library_app.js
+++ library_management/library_management/page/library_app/library_app.js
@@ -11,12 +11,29 @@ PageContent = Class.extend({
 		});
 		this.make();
 	},
-
+	
 	make: function () {
 		let htmlContent = `
-		
-		
-		
+			<div clas="container">
+				<div class="row">
+					<div class="col-md-4"> 
+						<div class="card" style="width: 18rem;">
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="#" class="btn btn-primary">Go somewhere</a>
+						</div>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="col-md-4">
+					<h2>Another piece of content</h2>
+        		</div>
+
+
+			</div>
 		`;
 
 		// add the page to the parent DOM element which is the entire page
