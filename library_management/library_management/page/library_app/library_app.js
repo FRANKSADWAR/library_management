797495afb7da82125b664e04ad9b439fb0a45c43
FRANKSADWAR <billyfranks98@gmail.com>
2023-04-20 14:00:57 +0300

    firmatting

--- library_management/library_management/page/library_app/library_app.js
+++ library_management/library_management/page/library_app/library_app.js
@@ -20,7 +20,7 @@ PageContent = Class.extend({
 						<div class="card" style="width: 18rem;">
 						<div class="card-body">
 							<h5 class="card-title">Card title</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card"s content.</p>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
 							<a href="#" class="btn btn-primary">Go somewhere</a>
 						</div>
 						</div>
@@ -31,10 +31,7 @@ PageContent = Class.extend({
 				<div class="col-md-4">
 					<h2>Another piece of content</h2>
         		</div>
-
-
-			</div>
-		`;
+			</div>`;
 
 		// add the page to the parent DOM element which is the entire page
 		$(frappe.render_template(htmlContent, this)).appendTo(this.page.main);
