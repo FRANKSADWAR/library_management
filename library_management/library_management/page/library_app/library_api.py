7eefa032787f748b6b0a68c8c6bb2a4ad2585651
FRANKSADWAR <billyfranks98@gmail.com>
2023-04-28 13:20:55 +0300

    all functions can be writtennas API's and used accordingly

--- library_management/library_management/page/library_app/library_api.py
+++ library_management/library_management/page/library_app/library_api.py
@@ -5,3 +5,18 @@ def get_article_count():
     articles = frappe.db.sql(""" SELECT count(*) AS articleNumbers FROM `tabArticle` """,as_dict=True)
     return articles
 
+@frappe.whitelist(allow_guest=True)
+def get_users():
+    users = frappe.db.get_list()
+    pass
+
+
+## An SQL quesry to get the Weekly sales from sales invoices, which have been paid
+@frappe.whitelist(allow_guest=True)
+def get_weekly_sales():
+    sales = frappe.db.sql(f"""  SELECT week(posting_date) AS week, SUM(grand_total) AS Weekly_sales FROM `tabSales Invoice` WHERE status='Paid' 
+                            GROUP BY week ORDER BY week; """, as_dict=True)
+
+
+    return sales                        
+
