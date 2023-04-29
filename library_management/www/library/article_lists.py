3704d9c4dbce4805f52a98404f14a9b88fddec18
FRANKSADWAR <billyfranks98@gmail.com>
2023-04-29 23:28:54 +0300

    get the context, it's nice

--- library_management/www/library/article_lists.py
+++ library_management/www/library/article_lists.py
@@ -1,4 +1,11 @@
 import frappe
 
+def get_articles():
+    articles = frappe.db.get_list('Article',fields=['article_name','author','status'])
+    return articles
+
 def get_context(context):
-    pass
+    context.art_ = get_articles()
+    return context
+
+
