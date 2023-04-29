e99e04d4c80c28755a89255ae1789b32e4ae7dd1
FRANKSADWAR <billyfranks98@gmail.com>
2023-04-29 23:59:34 +0300

    getting the list of members'

--- library_management/www/library/article_lists.py
+++ library_management/www/library/article_lists.py
@@ -4,8 +4,15 @@ def get_articles():
     articles = frappe.db.get_list('Article',fields=['article_name','author','status'])
     return articles
 
+def get_member():
+    members = frappe.db.get_list('Library Member',fields=['full_name'])
+    return members
+
 def get_context(context):
-    context.art_ = get_articles()
+    context = {
+        "art_" : get_articles()
+    }
+    
     return context
 
 
