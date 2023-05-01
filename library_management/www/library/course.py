8651066da8618a6b9d0fa87c8aed5a85bae240de
FRANKSADWAR <billyfranks98@gmail.com>
2023-05-01 14:10:00 +0300

    get the article and pass to Jinja template

--- library_management/www/library/course.py
+++ library_management/www/library/course.py
@@ -1,5 +1,14 @@
 import frappe
 
+## This is how a single data is obtained from a database and parsed to the Jinja template, we also use a web_route_rule hook 
 def get_context(context):
-    article = frappe.form_dict
+    article_name = frappe.form_dict.name
+    article = frappe.get_doc("Article",article_name)
     print("\n\n\n\n\n {} \n\n".format(article))
+    
+    context = {
+        "article": article
+    }
+    return context
+
+    
