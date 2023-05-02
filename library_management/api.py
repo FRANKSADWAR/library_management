fb40df272cdd4182ce2b166b7bea4ff5c015dff1
FRANKSADWAR <billyfranks98@gmail.com>
2023-05-02 11:31:34 +0300

    this is how pagination is done in frappe

--- library_management/api.py
+++ library_management/api.py
@@ -61,7 +61,31 @@ def paginate(doctype, page=0):
 
     query = f""" SELECT name, creation, modified, article_name, author, publisher, status FROM `tab{doctype}` {conditions};"""
 
-    if page:
-        pass
+    ## Thic condition is used when there is a page existing in the link already i.e articles/?page=5
+    if (page):
+        page = int(page)
+        ## get the articles depending on the page that we are at given the number of pagination that we want
+        articles = frappe.db.sql(query+f""" LIMIT {(page*3)-3}, 3""", as_dict=True)
+        ## next_page represents what exists after three pages
+        next_page = frappe.db.sql(query+f""" LIMIT {page*3}, 3""", as_dict=True)
+
+        ## check whether there is a next page 
+        if(next_page):
+            prev, next = page-1, page+1
+        else:
+            prev, next = page-1,0
+
+    ## This condition runs when the page is first loaded, i.e there is no page link that has been visited
     else:
-        count = frappe.db.sql(f""" SELECT COUNT(name) AS count  FROM `tab{doctype}` """,as_dict=True)[0].count
+        count = frappe.db.sql(f""" SELECT COUNT(name) AS count  FROM `tab{doctype}`; """,as_dict=True)[0].count
+        if (count > 3):
+            prev, next = 0,2
+        else:
+            pass    
+        articles = frappe.db.sql(query+ """ LIMIT 3""", as_dict=True)
+
+    return  {
+        "articles" : articles,
+        "prev" : prev,
+        "next" : next
+    }    
