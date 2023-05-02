ea9fa7fd79a72164d9b80dd30126e3822aa17db0
FRANKSADWAR <billyfranks98@gmail.com>
2023-05-02 13:05:01 +0300

    added the pafes

--- library_management/www/library/index.py
+++ library_management/www/library/index.py
@@ -3,10 +3,14 @@ from library_management.api import paginate
 
 def get_context(context):
     #articles = frappe.db.sql(f""" SELECT * FROM `tabArticle` ORDER BY creation DESC; """, as_dict=True)
+    
+    ## get the page from form_dict
+    page = frappe.form_dict.page
 
     ## call the pagination function which returns a dictionary 
-    pagination = paginate()
-    articles = pagination.get('articles') ## get the articles key 
+    pagination = paginate('Article',page=page)
+
+    articles = pagination.get("articles") ## get the articles key 
     next = pagination.get('next')
     prev = pagination.get('prev')
 
