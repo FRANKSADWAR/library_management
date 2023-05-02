6666d5ab99dc8eaab44a4dd066bc46eb0a7870e1
FRANKSADWAR <billyfranks98@gmail.com>
2023-05-02 10:56:06 +0300

    context in web generators

--- library_management/library_management/doctype/courses/courses.py
+++ library_management/library_management/doctype/courses/courses.py
@@ -1,14 +1,19 @@
 # Copyright (c) 2023, Billy Adwar and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 
-class Courses(Document):
+class Courses(WebsiteGenerator):
 	def __init__(self):
 		self.course_name = str(self.course_name).upper()
 
 	def before_save(self):
 		if not (self.course_fee):
 			self.course_fee = 500
-			
+
+	def get_context(self,context):
+		context = {
+			"course": frappe.db.get_value("Courses",self.course_name,["course_name as name","course_fee"],as_dict=True)
+		}
+		return context
