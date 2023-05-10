cab42adfebb4808b12985f096972ec7f7d8ad312
FRANKSADWAR <billyfranks98@gmail.com>
2023-05-10 08:30:55 +0300

    added this files

--- loan_management_customs.py
+++ loan_management_customs.py
@@ -1,4 +1,8 @@
 import frappe
 
-def get_filtered_customers():
-    pass
+@frappe.whitelist()
+def get_stock_order_items(sales_order):
+    sales_order_items = frappe.db.sql(""" SELECT item.item_code, item.item_name, item.description, item.qty, item.rate, item.amount 
+                        FROM `tabSales Order Item` AS item WHERE item.parent = %s """,(sales_order),as_dict=True)
+    return sales_order_items
+
