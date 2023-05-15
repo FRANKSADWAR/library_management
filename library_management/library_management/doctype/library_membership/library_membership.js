d10e87c4b63f4f89d61e19b8ab66144da8850a5b
billyfranks98@gmail.com <billyfranks98@gmail.com>
2023-05-15 13:59:08 +0300

    added customer balance

--- library_management/library_management/doctype/library_membership/library_membership.js
+++ library_management/library_management/doctype/library_membership/library_membership.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Library Membership', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+	},
+	before_save: function(){
+		let customer = frm.doc.customer;
+		frappe.call({
+			method: "library_management.api.get_customer_balance",
+			args: {
+				"company": frm.doc.company,
+				"customer_name": frm.doc.customer
+			},
+			callback: function(response){
+				frm.set_value({
+					"account_balance": response.message['account_balance'],
+					"customer_balance": response.message['party_balance'],
+				})
+			}
+		});
+	}
 });
